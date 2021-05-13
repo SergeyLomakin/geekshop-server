3e967f262bb8e975035a3e9b9bfc1e4539428900
SergeyLomakin <sergeylomakinals@mail.ru>
2021-05-13 18:44:50 +0500

    added login_required

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -136,3 +136,5 @@ AUTH_USER_MODEL = 'authapp.User' \
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login/'
